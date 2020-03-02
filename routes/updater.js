e3bc2f1ae2e960110604a660dd46f9ce170f9a91
Eric van der Toorn <erictoorn@gmail.com>
2020-03-02 14:37:31 +0100

    switch to using headers

--- routes/updater.js
+++ routes/updater.js
@@ -16,17 +16,19 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     console.log("received request from " + req.ip)
-    signature = req.body["X-Hub-Signature"];
+    signature = req.header("X-Hub-Signature");
     console.log('signature is ' + signature);
     // hmac.update(req.body)
     
     console.log(req.body);
 
 
-    event_type = req.body["X-GitHub-Event"];
-    delivery_guid = req.body["X-GitHub-Delivery"];
+    event_type = req.header("X-GitHub-Event");
+    delivery_guid = req.header("X-GitHub-Delivery");
     console.log(event_type);
     console.log(delivery_guid);
+
+    
     res.send("starting updater.....")
 });
 console.log("the updater was run.")
