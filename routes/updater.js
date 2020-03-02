752e5fec09a508ede03801e7f914e5766042d42c
Eric van der Toorn <erictoorn@gmail.com>
2020-03-02 14:28:25 +0100

    some mod on the webhook

--- routes/updater.js
+++ routes/updater.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const crypto = require("crypto");
 
 const fs = require('fs')
 secrets = JSON.parse(fs.readFileSync("./config/secrets.json"));
+const hmac = crypto.createHmac('sha1', secrets.update_key);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.send("only post requests valid..").status(400)
 });
 
-var middleware = require("hmac-express")("sha1", secrets.update_key, "X-Hub-Signature")
+// var middleware = require("hmac-express")("sha1", secrets.update_key, "X-Hub-Signature")
 
-router.post('/', middleware, function(req, res, next) {
+router.post('/', function(req, res, next) {
+    console.log("received request from " + req.ip)
+    signature = req.body["X-Hub-Signature"];
+    console.log('signature is ' + signature);
+    // hmac.update(req.body)
     
+    console.log(req.body);
+
+
     event_type = req.body["X-GitHub-Event"];
     delivery_guid = req.body["X-GitHub-Delivery"];
     console.log(event_type);
